957af85f9ac601953d1dad8ce443fa747d63d297
grinikita <Grigoriev98>
2021-08-26 00:31:06 +0300

    Eslint + webpack
    1. Init webpack config
    2. npm i eslint-webpack-plugin -D
    3. plugins: [new ESLintPlugin(options)]
    4. show tree shaking

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: '/src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+    ],
+  },
+  plugins: [new ESLintPlugin()],
+};
