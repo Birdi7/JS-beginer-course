893f7a0fd8a436c0e11329c2209ac3c8835bb3ed
grinikita <Grigoriev98>
2021-09-02 17:37:26 +0300

    http requests init

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,30 @@
 import { hot } from 'react-hot-loader/root'
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom'
 import Header from './components/Header/Header'
 import { GlobalStyle } from './index.styles'
 import Form from './components/Form/Form'
 import Game from './components/Game/Game'
 
 function App() {
+  const [state, setState] = useState({})
+
   return (
     <>
       <GlobalStyle />
       <Header />
       <Router>
         <Switch>
-          <Route path="/" exact component={Form} />
-          <Route path="/game" component={Game} />
+          <Route path="/" exact>
+            <Form
+              onSubmit={({ name, age }) => {
+                setState({ name, age })
+              }}
+            />
+          </Route>
+          <Route path="/game">
+            <Game name={state.name} age={state.age} />
+          </Route>
           <Route>404</Route>
         </Switch>
       </Router>
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
+import { GameTitle, GameWrapper } from './Game.styles'
+import Loader from '../Loader/Loader'
+import Deck from './Deck'
+import Cards from './Cards'
 
-function Game() {
-  return <div>Game</div>
+function Game({ name, age }) {
+  const history = useHistory()
+  useLayoutEffect(() => {
+    if (!name || !age) {
+      history.replace('/')
+    }
+  }, [])
+
+  const [deckId, setDeckId] = useState()
+  const [cards, setCards] = useState([])
+  const [remaining, setRemaining] = useState(52)
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').then((response) => {
+      if (response.data.success === true) {
+        setDeckId(response.data.deck_id)
+        setRemaining(response.data.remaining)
+        setLoading(false)
+      }
+    })
+  }, [])
+
+  const handleDrawCard = () => {
+    setLoading(true)
+    axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`).then((response) => {
+      setRemaining(response.data.remaining)
+      setCards((prevCards) => [...prevCards, ...response.data.cards])
+      setLoading(false)
+    })
+  }
+
+  const [points, setPoints] = useState(0)
+
+  return (
+    <GameWrapper>
+      {loading && !deckId ? (
+        <Loader />
+      ) : (
+        <>
+          <GameTitle>Hello {name}!</GameTitle>
+          <Deck count={remaining} loading={loading} drawCard={handleDrawCard} />
+          <Cards cards={cards} />
+        </>
+      )}
+    </GameWrapper>
+  )
 }
 
 export default Game
