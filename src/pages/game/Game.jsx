3e0e4cfee91799927d841353213e3ca7b0876cb0
grinikita <Grigoriev98>
2021-09-16 00:28:16 +0300

    Add async actions into redux (redux-thunk)

--- src/pages/game/Game.jsx
+++ src/pages/game/Game.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { GameTitle, GameWrapper } from './Game.styles'
 import Loader from '../../components/ui/loader/Loader'
 import Deck from './components/Deck'
 import Cards from './components/Cards'
 import parseCardPoint from '../../utils/cards/parseCardsPoints'
 import cardsService from '../../services/cards.service'
+import { drawCard, initCards } from '../../store/cards/cards.actions'
 
 function Game() {
   const { name, age } = useSelector((state) => state.user)
@@ -17,32 +18,16 @@ function Game() {
     }
   }, [])
 
-  const [deckId, setDeckId] = useState()
-  const [cards, setCards] = useState([])
-  const [remaining, setRemaining] = useState(52)
-
-  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  const { deckId, remaining, cards, loading, points } = useSelector((state) => state.cards)
 
   useEffect(() => {
-    cardsService.generateDeck().then((data) => {
-      if (data.success === true) {
-        setDeckId(data.deck_id)
-        setRemaining(data.remaining)
-        setLoading(false)
-      }
-    })
+    dispatch(initCards())
   }, [])
 
-  const [points, setPoints] = useState(0)
   const handleDrawCard = () => {
     if (remaining > 0) {
-      setLoading(true)
-      cardsService.drawCardsFromDeck(deckId, 1).then((data) => {
-        setRemaining(data.remaining)
-        setCards((prevCards) => [...prevCards, ...data.cards])
-        setPoints((prevPoints) => prevPoints + parseCardPoint(data.cards[0].value))
-        setLoading(false)
-      })
+      dispatch(drawCard())
     }
   }
 
